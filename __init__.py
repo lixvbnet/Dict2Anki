10c1dc6ae4ff6179278494700e1cf9ef3f4f4619
lixvbnet <lixvbnet@gmail.com>
2024-07-14 21:58:35 +0800

    add var for easy debugging

--- __init__.py
+++ __init__.py
@@ -1,8 +1,34 @@
+import os
+import requests
+
+DEBUG = False
+try:
+    rc = os.system(r'grep -E -q "^export\s+DICT2ANKI_SSL_VERIFY=0" ~/.bashrc.local.sh')
+    if rc == 0:
+        DEBUG = True
+except Exception:
+    pass
+
 try:
     from aqt import mw
+    # import the "show info" tool from utils.py
+    from aqt.utils import showInfo, qconnect
     # import all of Qt GUI library
     from aqt.qt import *
     from .addon.addonWindow import Windows
+
+    def disable_ssl_check():
+        original_req = requests.Session.request
+
+        def request(*args, **kwargs):
+            kwargs['verify'] = False
+            return original_req(*args, **kwargs)
+        requests.Session.request = request
+
+    if DEBUG:
+        showInfo("[Dict2Anki] DEBUG=True, SSL Check is DISABLED!")
+        disable_ssl_check()
+
 
     def show_window():
         w = Windows()
